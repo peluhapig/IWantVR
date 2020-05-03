857dc70aaeae9990c4dc285be51dbe8e6133732d
peluhapig <ivanka.marie@gmail.com>
2020-05-03 18:59:56 -0400

    Made it check every hour

--- google.py
+++ google.py
@@ -4,24 +4,6 @@ import csv
 import time
 import smtplib , ssl
 from login import sender_email, receiver_email, password
-
-driver = webdriver.Chrome(executable_path='chromedriver.exe')
-with open("websiteList.csv") as f:
-    reader = csv.reader(f)
-    siteList = list(reader)
-    #print(siteList)
-    for i in siteList:
-        driver.get(i[0])
-        time.sleep(4)
-        xPath = i[1]
-        text = driver.find_element_by_xpath(xPath).text
-        if text != i[2]:
-            print("negative")
-        else:
-            email(i[0])
-
-
-
 def email(website):
 
     message = "Check this out this is the link:" + website
@@ -31,3 +13,23 @@ def email(website):
     server.login(sender_email, password)
 
     server.sendmail(sender_email, receiver_email, message)
+
+while True: 
+
+    driver = webdriver.Chrome(executable_path='chromedriver.exe')
+    with open("websiteList.csv") as f:
+        reader = csv.reader(f)
+        siteList = list(reader)
+        #print(siteList)
+        for i in siteList:
+            driver.get(i[0])
+            time.sleep(4)
+            xPath = i[1]
+            text = driver.find_element_by_xpath(xPath).text
+            if text != i[2]:
+                print("negative")
+            else:
+                email(i[0])
+    time.sleep(60*60)
+
+
