0529d26a8592b878a69ff5646e786f7d2923397a
peluhapig <ivanka.marie@gmail.com>
2020-05-08 11:53:38 -0400

    changed hour to 15 mins

--- test.py
+++ test.py
@@ -4,6 +4,10 @@ import csv
 import time
 import smtplib , ssl
 from login import sender_email, receiver_email, password
+import requests
+from lxml import html, etree
+
+
 def email(website):
 
     message = "Check this out this is the link:" + website
@@ -14,35 +18,21 @@ def email(website):
 
     server.sendmail(sender_email, receiver_email, message)
 
-def checkExists(xpath):
-    try:
-        driver.find_element_by_xpath(xpath)
-    except NoSuchElementException:
-        return False
-    return True
-
 while True: 
 
-    driver = webdriver.Chrome(executable_path='chromedriver.exe')
     with open("websiteList.csv") as f:
+        
         reader = csv.reader(f)
         siteList = list(reader)
         #print(siteList)
         for i in siteList:
-            driver.get(i[0])
+            page = requests.get(i[0])
             time.sleep(4)
             xPath = i[1]
-            exists = checkExists(xPath)
-            if exists == True: 
-                text = driver.find_element_by_xpath(xPath).text
-                if text != i[2]:
-                    print("negative")
-                else:
-                    print("positive")
-                    email(i[0])
+            extractedHtml = html.fromstring(page.content)
+            text = extractedHtml.xpath(xPath)[0].text
+            if text != i[2]:
+                print("negative")
             else:
-                print("positive")
                 email(i[0])
-    time.sleep(15*60)
-
-
+    time.sleep(60*60)
